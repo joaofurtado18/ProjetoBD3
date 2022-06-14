6bf84c200747f3c853ae4dc7c37e6d9288e60ffa
joaofurtado18 <joao.melo.furtado@tecnico.ulisboa.pt>
2022-06-14 13:52:39 +0100

    Ponto 1 feito

--- Pergunta1.sql
+++ Pergunta1.sql
@@ -1,9 +1,19 @@
-drop table borrower cascade;
-drop table loan cascade;
-drop table depositor cascade;
-drop table account cascade;
-drop table customer cascade;
-drop table branch cascade;
+drop table category cascade;
+drop table simple_category cascade;
+drop table super_category cascade;
+drop table has_other cascade;
+drop table product cascade;
+drop table has_category cascade;
+drop table IVM cascade;
+drop table retail_point cascade;
+drop table instaled_at cascade;
+drop table shelf cascade;
+drop table planogram cascade;
+drop table retailer cascade;
+drop table responsable_for cascade;
+drop table replenishment_event cascade;
+
+
 
 ----------------------------------------
 -- Table Creation
@@ -25,6 +35,11 @@ create table simple_category
 create table super_category
     (constraint pk_super_category primary key(super_category_name)
     constraint fk_super_category foreign key(category) references category(category_name))
+
+create table has_other
+    (constraint fk_has_other_super_category foreign key(super_category) references super_category
+    constraint fk_has_other_category foreign key(category) references category)
+
 
 create table product
    (EAN 	        numeric(13, 0)	not null unique,
@@ -63,7 +78,7 @@ create table planogram
     units   numeric
     loc     varchar(80)
     constraint fk_planogram_EAN foreign key(product) references product(EAN)
-    constraint fk_planogram foreign key(shelf) references shelf(fk_shelf, shelf_number))
+    constraint fk_planogram_shelf foreign key(shelf) references shelf(fk_shelf, shelf_number))
 
 create table retailer
     (TIN            numeric(9, 0) not null unique
@@ -71,77 +86,15 @@ create table retailer
 
 create table responsable_for
    (constraint fk_responsable_for_IVM foreign key(IVM) references IVM(serial_number, manuf)
-   constraint fk_responsable_for_TIN foreign key retailer references retailer(TIN)
+   constraint fk_responsable_for_TIN foreign key(retailer) references retailer(TIN)
    constraint fk_responsable_for_category foreign key(category) references category(category_name))
+
+create table replenishment_event
+    (constraint fk_replenishment_event_planogram foreign key(planogram) references planogram(fk_planogram_EAN, fk_planogram_shelf)
+    constraint fk_replenishment_event_retailer foreign key(retailer) references retailer(TIN))
 
 ----------------------------------------
 -- Populate Relations 
 ----------------------------------------
 
-insert into customer values ('Adams',	'Main Street',	'Lisbon');
-insert into customer values ('Brown',	'Main Street',	'Oporto');
-insert into customer values ('Cook',	'Main Street',	'Lisbon');
-insert into customer values ('Davis',	'Church Street','Oporto');
-insert into customer values ('Evans',	'Forest Street','Coimbra');
-insert into customer values ('Flores',	'Station Street','Braga');
-insert into customer values ('Gonzalez','Sunny Street', 'Faro');
-insert into customer values ('Iacocca',	'Spring Steet',	'Coimbra');
-insert into customer values ('Johnson',	'New Street',	'Cascais');
-insert into customer values ('King',	'Garden Street','Aveiro');
-insert into customer values ('Lopez',	'Grand Street',	'Vila Real');
-insert into customer values ('Martin',	'Royal Street',	'Braga');
-insert into customer values ('Nguyen',	'School Street','Castelo Branco');
-insert into customer values ('Oliver',	'First Stret',	'Oporto');
-insert into customer values ('Parker',	'Hope Street',  'Oporto');
-
-insert into branch values ('Central',	'Guimarães',		2100000);
-insert into branch values ('Clerigos',  'Oporto',		3900000);
-insert into branch values ('Downtown',	'Lisbon',		1900000);
-insert into branch values ('Metro',	'Amadora',	 	400200);
-insert into branch values ('Round Hill','Amadora',		8000000);
-insert into branch values ('Ship Terminal', 'Sintra',	 	0400000);
-insert into branch values ('University',	'Vila Real',	7200000);
-insert into branch values ('Uptown',	'Amadora',		1700000);
-insert into branch values ('Wine Celar', 'Oporto',		4002800);
-
-insert into account values ('A-101',	'Downtown',	500);
-insert into account values ('A-102',	'Uptown',	700);
-insert into account values ('A-201',	'Uptown',	900);
-insert into account values ('A-215',	'Metro',	600);
-insert into account values ('A-217',	'University',	650);
-insert into account values ('A-222',	'Central',	550);
-insert into account values ('A-305',	'Round Hill',	800);
-insert into account values ('A-333',	'Central',	750);
-insert into account values ('A-444',	'Downtown',	850);
-
-insert into depositor values ('Cook',	 'A-102');
-insert into depositor values ('Johnson', 'A-101');
-insert into depositor values ('Cook',	 'A-101');
-insert into depositor values ('Johnson', 'A-201');
-insert into depositor values ('Brown',	 'A-215');
-insert into depositor values ('Iacocca', 'A-217');
-insert into depositor values ('Evans',   'A-222');
-insert into depositor values ('Flores',	 'A-305');
-insert into depositor values ('Oliver',  'A-333');
-insert into depositor values ('Brown',	 'A-444');
-
-insert into loan values ('L-11', 'Round Hill',	6000);
-insert into loan values ('L-14', 'Downtown',	4000);
-insert into loan values ('L-15', 'Uptown',	3000);
-insert into loan values ('L-16', 'Uptown',	7000);
-insert into loan values ('L-17', 'Downtown',	1000);
-insert into loan values ('L-20', 'Downtown',	8000);
-insert into loan values ('L-21', 'Central',	9000);
-insert into loan values ('L-23', 'Central',	2000);
-insert into loan values ('L-93', 'Metro',	5000);
-
-insert into borrower values ('Brown',	'L-11');
-insert into borrower values ('Nguyen',	'L-14');
-insert into borrower values ('Cook',	'L-15');
-insert into borrower values ('Iacocca',	'L-16');
-insert into borrower values ('Gonzalez','L-17');
-insert into borrower values ('Iacocca',	'L-17');
-insert into borrower values ('Parker',	'L-20');
-insert into borrower values ('Brown',	'L-21');
-insert into borrower values ('Brown',	'L-23');
-insert into borrower values ('Davis',	'L-93');
+-- inserts aqui
